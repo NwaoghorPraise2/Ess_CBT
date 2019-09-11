b889b597296396799f0d4386d61fb43ead00cabb
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2019-09-11 16:18:50 +0100

    change made

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = 4500;
 
 app.use(morgan('short'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // Just trying to see if the base works
@@ -18,7 +18,8 @@ app.use(express.static('./public UI'));
 
 //routes
 const getBooks = (request, response) => {
-    pool.query("SELECT * FROM questions", (error, results) => {
+    const id = request.params.id;
+    pool.query("SELECT * FROM questions",(error, results) => {
         if (error) {
             throw error;
         }
@@ -49,20 +50,21 @@ app.post('/add_question', (req, res) => {
 
 app.get("/questions",getBooks);
 
-app.get('/questions/:id', (req, res) => {
-const ID = req.params.id; 
-const QueryString = 'SELECT * FROM questions WHERE id = $1';
-pool.query(QueryString, [ID] , error => {
+app.get('/questions/:id', (request, response) => {
+    const id = request.params.id;
+    pool.query("SELECT * FROM questions WHERE id = $1",[id] ,(error, results) => {
+        if (error) {
+            throw error;
+        }
 
-    if(error){
-    //alert('error occupied' + error);
-    console.log(error);
-    }
-    res.status(200);
+        response.status(200).json(results);
+    });
 });
-});
+
+
 
 
 app.listen(process.env.PORT || port, () => {
     console.log(`server started on ${port}`);
 });
+
